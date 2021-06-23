b67d2a6baee89ec8a19f341fcd2b8746b5fd5968
Abdulrahman Yusuf <abdulrahmanyusuf125@gmail.com>
2021-06-23 13:13:24 +0100

    update

--- src/lib/helpers/Request.js
+++ src/lib/helpers/Request.js
@@ -10,8 +10,8 @@ token.subscribe((token) => {
 export class Request {
   constructor() {
     const request = axios.create({
-      // baseURL: "https://tornad.herokuapp.com/",
-      baseURL: "http://localhost:5000/",
+      baseURL: "https://tornad.herokuapp.com/",
+      // baseURL: "http://localhost:5000/",
       headers: {
         "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${authToken}`,
